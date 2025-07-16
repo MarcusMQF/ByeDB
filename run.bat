da233d6f8e980489388e9918a699c49500cb1702
StanX19 <tanshanchien@gmail.com>
2025-07-17 01:12:33 +0800

    run

---
+++ run.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Starting frontend...
+start /min cmd /k "cd frontend && npm run dev"
+
+echo Starting backend...
+start /min cmd /k "cd backend && uvicorn main:app --reload"
+
+echo Both frontend and backend are starting in separate windows.
+echo local:     http://localhost:3000/
