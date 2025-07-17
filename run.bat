21e592f72b582bde6709568951d86034b516f911
StanX19 <tanshanchien@gmail.com>
2025-07-18 00:14:55 +0800

    run bat

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Starting frontend...
-start /min cmd /k "cd frontend && npm run dev"
+start /min cmd /k "cd frontend && npm install && npm run dev"
 
 echo Starting backend...
 start /min cmd /k "cd backend && uvicorn main:app --reload"
